8ac31b316998f887c78e18236868f6e8934f7a9f
SuyashD02 <suyashdattawade007@gmail.com>
2024-01-18 00:44:51 +0530

    Hotel Detail Ui

--- src/Page/Hotels/HotelDetail.js
+++ src/Page/Hotels/HotelDetail.js
@@ -143,10 +143,10 @@ function HotelDetail() {
             </div>
             <div className={Classes.hotelDetailNavigateSection}>
               <div className={Classes.hotelDetailNav}>
-                <p>Rooms</p>
+                <p className={Classes.hotelDetailInfoNav}>Rooms</p>
               </div>
               <div className={Classes.hotelDetailNav}>
-                <p>Amenities</p>
+                <p className={Classes.hotelDetailInfoNav}>Amenities</p>
               </div>
             </div>
             <div className={Classes.hotelDetailRoomsSection}>
@@ -154,14 +154,76 @@ function HotelDetail() {
                 <div className="h-[100%] w-[25%] border border-blue-500 border-solid flex items-center">
                   <p className="ml-[20px]">Room Type</p>
                 </div>
-                <div className="h-[100%] w-[25%] border border-blue-500 border-solid flex items-center">
+                <div className="h-[100%] w-[30%] border border-blue-500 border-solid flex items-center">
                   <p className="ml-[20px]">Benefits</p>
                 </div>
-                <div className="h-[100%] w-[50%] border border-blue-500 border-solid flex items-center justify-center">
+                <div className="h-[100%] w-[45%] border border-blue-500 border-solid flex items-center justify-center">
                   <p>Per Night Price</p>
                 </div>
 
 
+              </div>
+              <div className="border border-red-500 border-solid w-[100%] h-[100%] flex flex-col">
+                <div className="border border-blue-500 border-solid w-[100%] h-[40%] flex">
+                  <div className="h-[100%] w-[25%] border border-green-500 border-solid flex justify-center items-center">
+                    <div className="border border-red-500 border-solid w-[95%] h-[95%] flex flex-col gap-[10px]">
+                      <div className="w-[100%] h-[15%] flex items-center">
+                      <p className="text-[15px] font-[600] text-[#000]">{hotelDetailData.rooms?.[0].roomType} Room</p>
+                      </div>
+                      <div className="w-[100%] h-[70%] rounded-[5px]" >
+                      <img className="w-[100%] h-[100%] object-cover rounded-[5px]" src={hotelDetailData?.images?.[0]}/>
+                      </div>
+                      <div className=" w-[100%] h-[15%] flex gap-[8px]">
+                        <div className=" w-[35%] h-[100%] bg-[#D5E5FA] rounded-[5px] text-[13px] flex justify-center items-center">
+                          <p>{hotelDetailData.rooms?.[0].bedDetail}</p>
+                        </div>
+                        <div className=" w-[30%] h-[100%] bg-[#D5E5FA] rounded-[5px] text-[13px] flex justify-center items-center">
+                        <p>{hotelDetailData.rooms?.[0].roomSize} sq.ft</p>
+                        </div>        
+                        </div>
+                    </div>
+                  </div>
+                  <div className="h-[100%] w-[30%] border border-yellow-500 border-solid flex justify-center items-center">
+                    <div className="border border-red-500 border-solid w-[95%] h-[95%]">
+                      <div className="w-[100%] h-[15%] flex items-center gap-[5px]">
+                        <p className="h-[18px] w-[4px] rounded-[3px] bg-[#0c6be9] "></p>
+                        <p className="text-[14px] font-[600] text-[#000]">Room Only</p>
+                      </div>
+                      <div className="w-[100%] h-[50%]">
+                        <div className="ml-[10px] h-[30%] text-[#000] text-[13px] flex gap-[5px] items-center" >
+                          <img className="w-[13px] h-[13px]" src="https://flight.easemytrip.com/Content/img/tick1.svg"/>
+                          <p>{hotelDetailData.rooms?.[0].cancellationPolicy}</p>
+                        </div>
+                        <div className="ml-[10px] h-[30%] text-[#000] text-[13px] flex gap-[5px] items-center" >
+                          <img className="w-[13px] h-[13px]" src="https://flight.easemytrip.com/Content/img/tick1.svg"/>
+                          <p>Breakfast not included</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[100%] w-[45%] border border-orange-500 border-solid flex items-center gap-[15px]">
+                    <div className="border border-blue-500 border-solid h-[95%] w-[60%] flex">
+                      <div className="h-[100%] w-[20%]">
+                        <img className="w-[60px]" src="https://hotel.easemytrip.com/Images/Hotel/icon/recommded-3.svg"/>
+                      </div>
+                      <div className="w-[80%] h-[60%]">
+                      <div className="mt-[10px] flex items-center justify-end gap-[1px] w-[100%] h-[5vh] text-[24px] font-[600] ">
+                        <img src="https://hotels.easemytrip.com/newhotel/Content/img/rupee_new_black.svg"/>
+                        <p className="mr-[2px]">{hotelDetailData.rooms?.[0].costPerNight}</p>
+                      </div>
+                      <div className="flex items-center justify-end text-[12px] font-[550] text-[#000] w-[100%] h-[20%] ">
+                    <p>+ </p>
+                    <img className={Classes.resIconHotel} src="https://hotels.easemytrip.com/newhotel/Content/img/rupee_new_black.svg"/>
+                    <p className="mr-[2px]"> {hotelDetailData.rooms?.[0].costDetails.taxesAndFees} Taxes & fees</p>
+                    
+                  </div>
+                  <p className="text-[12px] text-[#737373] mr-[2px] flex  justify-end ">(Per Night)</p>
+
+                    </div>
+                    </div>
+                    <div className="border border-blue-500 border-solid h-[95%] w-[37%]"></div>
+                  </div>
+                </div>
               </div>
             </div>
       </div>
